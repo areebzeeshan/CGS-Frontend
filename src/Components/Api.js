918463938c6b73616609a02c1f53f991b7c01e4d
areebzeeshan <areebzeeshan03@gmail.com>
2024-03-02 00:36:41 +0500

    update

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,5 +1,5 @@
 // local server
-// http://localhost:4000
-const api = "https://cgs-backend-qeer.onrender.com";
+// https://cgs-backend-qeer.onrender.com
+const api = "http://localhost:4000";
 
 export default api; 
