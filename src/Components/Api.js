dfd2eb8ba4e9d9b3eea7a6e579c1e4a4a7d3d2fd
Areeb Zeeshan <88983748+areebzeeshan@users.noreply.github.com>
2024-05-15 20:17:22 +0500

    Update Api.js

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,7 +1,7 @@
 // local server
 // http://localhost:4000
-// const api = "https://cgs-backend-qeer.onrender.com";
-const api = "http://localhost:4000";
+const api = "https://cgs-backend-qeer.onrender.com";
+// const api = "http://localhost:4000";
 // const api = "http://103.245.195.191:4000";
 
 
